b4d8a86b4a327abf32010fa1b97b3638931cb154
lifeee <1244753906@qq.com>
2026-08-08 19:52:12 +0800

    Expand Android course: lib/complete-chapter-content.ts

--- lib/complete-chapter-content.ts
+++ lib/complete-chapter-content.ts
@@ -1,10 +1,16 @@
 import { androidEngineeringContent } from "./content-android-engineering";
+import { composeContent } from "./content-compose";
 import { coroutinesFlowContent } from "./content-coroutines-flow";
+import { jetpackContent } from "./content-jetpack";
 import { languageContent } from "./content-language";
+import { productionAndroidContent } from "./content-production-android";
 import type { CompleteChapterContent } from "./content-types";
 
 export const completeChapterContent: Record<string, CompleteChapterContent> = {
   ...languageContent,
   ...coroutinesFlowContent,
   ...androidEngineeringContent,
+  ...jetpackContent,
+  ...composeContent,
+  ...productionAndroidContent,
 };
